bce6731adec0875ee205bcb24f0ff9ead5ebb073
Patricio Gonzalez <pato@dekaz.cl>
2020-03-07 16:19:54 -0300

    post instagram

---
+++ instagramfeed/src/App.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  #app
+    h2 Breathe Code Photo Feed
+    .container
+      post-card(title="This is my title", url="https://picsum.photos/600/400", desc="Eiusmod enim sunt velit tempor ullamco dolore fugiat pariatur magna dolor excepteur do est.")
+      post-card(title="This is my title2", url="https://picsum.photos/600/390", desc="Eiusmod enim sunt velit tempor ullamco dolore fugiat pariatur magna dolor excepteur do est.")
+      post-card(title="This is my title3", url="https://picsum.photos/600/400", desc="Eiusmod enim sunt velit tempor ullamco dolore fugiat pariatur magna dolor excepteur do est.")
+</template>
+
+<script>
+import postCard from '@/components/post-card.vue'
+
+export default {
+  name: 'app',
+  components: { postCard },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css?family=Oswald:400,500,600,700&display=swap')
+#app 
+  font-family: 'Oswald', sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+  h2
+    font-family: 'Oswald', sans-serif
+    font-size: 2em
+    text-align: center
+  .container
+    background: #EAEAEA
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+
+
+</style>
---
+++ instagramfeed/src/components/post-card.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+    .card
+        .head
+            .title {{ title }}
+            .number 10/14
+        img( :src="url") 
+        .desc {{ desc }}
+</template>
+<script>
+export default {
+     props: {
+    parentData: Object,
+    stringProp: String,
+    title: String,
+    url: String,
+    desc: String
+  }
+}
+</script>
+<style lang="sass">
+    .card
+        display: flex
+        flex-direction: column
+        color: white
+        border-radius: 5px
+        max-width: 500px
+        margin: 30px 0px
+        box-shadow: 2px 2px 4px 0px #bfbfbf
+        background: white
+        .head
+            display: flex
+            justify-content: space-between
+            flex-direction: row
+            padding: 20px 10px
+            .title
+                font-weight: bold
+                color: black
+                font-family: 'Oswald', sans-serif
+            .number
+                color: gray
+                font-family: 'Oswald', sans-serif
+        img
+        .desc
+            padding: 20px 10px
+            color: black
+            font-family: 'Oswald', sans-serif
+</style>
