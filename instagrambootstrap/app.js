426f7d2922bf5eaa83d6c7337f1f6b96337ebaab
Patricio Gonzalez <pato@dekaz.cl>
2020-03-21 07:16:15 -0300

    fix instagram bootstrap

--- instagrambootstrap/app.js
+++ instagrambootstrap/app.js
@@ -1,17 +1,24 @@
 const btn1 = document.querySelector('.iconos.op1')
 const btn2 = document.querySelector('.iconos.op2')
 const posts = document.querySelectorAll('.post')
+const title = document.querySelectorAll('.post .title')
+const text = document.querySelectorAll('.post .text')
 
 btn2.addEventListener('click', ()=>{
- posts.forEach(post => {
+ posts.forEach((post, index) => {
      post.classList.remove('col-4')
      post.classList.add('col-12')
+     title[index].classList.add('active')
+     text[index].classList.add('active')
  })
 })
 
 btn1.addEventListener('click', ()=>{
-    posts.forEach(post => {
+    posts.forEach((post, index) => {
     post.classList.add('col-4')
      post.classList.remove('col-12')
+     title[index].classList.remove('active')
+     text[index].classList.remove('active')
+     
     })
 })
